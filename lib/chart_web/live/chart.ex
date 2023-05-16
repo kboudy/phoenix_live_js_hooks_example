2a22afe6ab7ef3e44dd559165869a801e8a8bb85
Keith Boudreau <kboudy@gmail.com>
2023-05-16 15:33:01 -0400

    click interaction round trip working

--- lib/chart_web/live/chart.ex
+++ lib/chart_web/live/chart.ex
@@ -1,12 +1,32 @@
 defmodule ChartWeb.ChartLive.Index do
   use ChartWeb, :live_view
 
-  @impl true
+  def mount(_params, _session, socket) do
+    {:ok,
+     assign(socket,
+       # note: not using atom keys here,
+       #       because json will be converted to a map with string keys
+       clicked_coords: nil
+     )}
+  end
+
   def render(assigns) do
     ~H"""
-    <div class="chart">
-      ChartWeb.ChartLive.Index
+    <div id="chartjs-demo">
+      <div phx-update="ignore" id="chartjs-wrapper">
+        <canvas id="line-chart" phx-hook="Chart"></canvas>
+      </div>
+
+      <div :if={@clicked_coords} class="text-center mt-10">
+        <%= "you clicked: #{@clicked_coords["x"]}, #{@clicked_coords["y"]}" %>
+      </div>
     </div>
     """
+  end
+
+  def handle_event("chart-clicked", coords, socket) do
+    socket = assign(socket, clicked_coords: coords)
+
+    {:noreply, socket}
   end
 end
